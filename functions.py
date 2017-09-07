eb2bd26ef1d3273d0005208ddecf4a5b8fe8f027
Julian Yaman <julianyaman@gmx.de>
2017-09-08 01:22:27 +0200

    Functions

---
+++ functions.py
@@ -0,0 +1,11 @@
+def area(radius):
+    return 3.142 * radius * radius
+
+def vol(area, length):
+    print(area * length)
+
+radius = int(input("Enter a radius: "))
+length = int(input("Enter a length: "))
+
+vol(area(radius), length)
+
