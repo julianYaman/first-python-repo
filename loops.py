1c438c773a4e1ce27fc5a332065efafa0ed6101c
Julian Yaman <julianyaman@gmx.de>
2017-09-08 01:11:59 +0200

    Ranges + While loop

--- loops.py
+++ loops.py
@@ -1,4 +1,21 @@
-names = ["Julian", "Johannes", "Fabian", "Kevin", "Marius"]
+# FOR LOOPS
 
-for name in names:
-    print(name)
+# names = ["Julian", "Johannes", "Fabian", "Kevin", "Marius"]
+#
+# for name in names:
+#     print(name)
+
+age = 25
+num = 0
+
+# WHILE LOOP
+
+while num < age:
+    if num == 0:
+        num += 1
+        continue
+    if num % 2 == 0:
+        print(num)
+    if num > 10:
+        break
+    num +=1
