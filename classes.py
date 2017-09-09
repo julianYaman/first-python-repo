43338bca4ce81746acd936a48d1f252d630f1fde
Julian Yaman <julianyaman@gmx.de>
2017-09-10 00:33:10 +0200

    Classes Advancing Init Function

--- classes.py
+++ classes.py
@@ -1,18 +1,23 @@
 class Planet:
 
-    def __init__(self):
-        self.name = 'Hoth'
-        self.radius = 200000
-        self.gravity = 5.5
-        self.system = "Hoth System"
+    def __init__(self, name, radius, gravity, system):
+        self.name = name
+        self.radius = radius
+        self.gravity = gravity
+        self.system = system
 
     def orbit(self):
         return f'{self.name} is orbiting in the {self.system}'
 
-hoth = Planet()
+hoth = Planet('Hoth', 200000, 5.5, "Hoth System")
 print(f'Name is: {hoth.name}')
 print(f'Radius is: {hoth.radius} ')
 print(f'The gravity is: {hoth.gravity} ')
 print(hoth.orbit())
 
+naboo = Planet("Naboo", 300000, 8, "Naboo Sytem")
+print(f'Name is: {naboo.name}')
+print(f'Radius is: {naboo.radius} ')
+print(f'The gravity is: {naboo.gravity} ')
+print(naboo.orbit())
 
